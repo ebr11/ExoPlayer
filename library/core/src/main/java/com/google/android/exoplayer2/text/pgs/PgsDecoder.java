dee2888cf9e4e8e4f880c44747602fc477d1cb61
Eric Reed <spam@reedsplace.com>
2017-08-08 18:21:47 -0400

    PGS Subs

---
+++ library/core/src/main/java/com/google/android/exoplayer2/text/pgs/PgsDecoder.java
@@ -0,0 +1,29 @@
+package com.google.android.exoplayer2.text.pgs;
+
+import android.util.Log;
+
+import com.google.android.exoplayer2.text.SimpleSubtitleDecoder;
+import com.google.android.exoplayer2.text.Subtitle;
+import com.google.android.exoplayer2.text.SubtitleDecoderException;
+import com.google.android.exoplayer2.util.ParsableByteArray;
+
+@SuppressWarnings("unused")
+public class PgsDecoder extends SimpleSubtitleDecoder {
+
+    @SuppressWarnings("unused")
+    public PgsDecoder() {
+        super("PgsDecoder");
+    }
+
+    @Override
+    protected Subtitle decode(byte[] data, int size, boolean reset) throws SubtitleDecoderException {
+        ParsableByteArray buffer = new ParsableByteArray(data, size);
+        Log.i("PgsDecoder","**** Decoding PGS subs. Size: "+Integer.toString(size));
+        PgsBuilder builder = new PgsBuilder();
+        do {
+            if (!builder.readNextSection(buffer))
+                break;
+        } while (buffer.bytesLeft() > 0);
+        return builder.build();
+    }
+}
