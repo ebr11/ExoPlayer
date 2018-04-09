19fcdbdf232770871c82fcc6d9188282c604aeb3
Eric Reed <spam@reedsplace.com>
2018-04-09 13:35:34 -0400

    Update version

--- library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerLibraryInfo.java
+++ library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerLibraryInfo.java
@@ -33,7 +33,7 @@ public final class ExoPlayerLibraryInfo {
 
   /** The version of the library expressed as {@code "ExoPlayerLib/" + VERSION}. */
   // Intentionally hardcoded. Do not derive from other constants (e.g. VERSION) or vice versa.
-  public static final String VERSION_SLASHY = "ExoPlayerLib/2.7.0/Emby";
+  public static final String VERSION_SLASHY = "ExoPlayerLib/2.7.3/Emby";
 
   /**
    * The version of the library expressed as an integer, for example 1002003.
